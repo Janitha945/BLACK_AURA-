8100423f395c53295e2c52d9307fd4f19afabe57
Janitha945 <janithasithum945@gmail.com>
2026-01-23 14:09:01 +0530

    Initial upload
    Initial upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: A simple message when the user clicks 'Buy Now'
+document.querySelector('.buy-button').addEventListener('click', function() {
+    console.log("Redirecting to checkout...");
+});
